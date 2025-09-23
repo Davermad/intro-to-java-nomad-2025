421cae6f2d00da816ec64616d9e3f91c3e637b0f
Daniel <danielwebd17@gmail.com>
2025-09-23 09:22:04 +0600

    fix error

--- lab-01/src/Problem03.java
+++ lab-01/src/Problem03.java
@@ -3,6 +3,6 @@ public class Problem03 {
         System.out.println("    J     A   V     V   A");
         System.out.println("    J    A A   V   V   A A");
         System.out.println("J   J   AAAAA   V V   AAAAA");
-        System.out.println(" JJ   A     A   V   A     A");
+        System.out.println(" JJ    A     A   V   A     A");
     }
 }
